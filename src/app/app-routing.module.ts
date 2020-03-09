a72c291ca8f839c7b6926ad10ccda1be1cbb3eb4
farley-melo <farleynirvana@gmail.com>
2020-03-09 00:26:20 -0300

    criacao do menu de navegacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CursosComponent} from './cursos/cursos.component';
+import {CursoDetalhesComponent} from './curso-detalhes/curso-detalhes.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'curso',component:CursosComponent},
+  {path:'curso/:idCurso',component:CursoDetalhesComponent},
+  {path:'',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalhesComponent } from './curso-detalhes/curso-detalhes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CursosComponent,
+    CursoDetalhesComponent
   ],
   imports: [
     BrowserModule,
